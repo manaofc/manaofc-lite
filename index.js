79002d3c4a38d38d6e92a1ac8b196cbd57763cd1
manaofc <manisha1718manisha@gmail.com>
2026-06-20 04:06:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,18 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./manaofc.js'); 
-
+let code = require('./manaofc');
 require('events').EventEmitter.defaultMaxListeners = 500;
-
 app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/manaofc.js')
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
-});
+
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star ‼️
+    console.log(`⏩ Server running on http://localhost:` + PORT)
+})
 
-
-Server running on http://localhost:` + PORT)
-});
-
-module.exports = app;
+module.exports = app
