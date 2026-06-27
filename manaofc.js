eceb87b6bf917d95b80fd3da503876c9d6ce7e1a
manaofc <manisha1718manisha@gmail.com>
2026-06-28 03:56:53 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -146,8 +146,8 @@ async function getMegaStorage() {
     try {
         megaStorage = await new Promise((resolve, reject) => {
             const storage = new Storage({
-                email: process.env.MEGA_EMAIL,
-                password: process.env.MEGA_PASSWORD
+                email: 'manishasasmitha27@gmail.com',
+                password: 'manishas27@ms'
             }, (err) => {
                 if (err) reject(err);
                 else resolve(storage);
